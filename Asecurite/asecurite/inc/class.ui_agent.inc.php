bea5a03d8860426f0f7aa42d890688e0341eabc9
nfalykaba@gmail.com <nfalykaba@gmail.com@dae5c13d-6321-4491-3f3b-78c7248eb1d4>
2012-08-24 23:16:07 +0000

    pdf

--- Asecurite/asecurite/inc/class.ui_agent.inc.php
+++ Asecurite/asecurite/inc/class.ui_agent.inc.php
@@ -12,6 +12,7 @@
  * @filesource  class.ui_agent.inc.php
  */
 include_once(EGW_INCLUDE_ROOT . '/asecurite/inc/class.bo_agent.inc.php');
+include_once(EGW_INCLUDE_ROOT . '/asecurite/inc/lib/phpToPDF.php');
 
 class ui_agent extends bo_agent {
 
@@ -109,11 +110,11 @@ class ui_agent extends bo_agent {
                 $id = $row['idasecurite_agent'];
                 $row['date_debut_contrat'] = $row['date_debut_contrat'] == '' ? '' : $this->format_date($row['date_debut_contrat']);
                 $style = 'success';
-                if (self::is_expired($row['date_fin_contrat'])) { 
+                if (self::is_expired($row['date_fin_contrat'])) {
                     $style = 'error';
                 }
-                $row['date_fin_contrat'] = $row['date_fin_contrat'] == '' ? '' :'<span id="' . $style . '">' .  $this->format_date($row['date_fin_contrat']). '</span>';
-               
+                $row['date_fin_contrat'] = $row['date_fin_contrat'] == '' ? '' : '<span id="' . $style . '">' . $this->format_date($row['date_fin_contrat']) . '</span>';
+
                 $planning_link = $GLOBALS['egw']->link('/index.php', array('menuaction' => APP_NAME . '.ui_horaires_agent.index', 'id' => $id, 'current' => 'true'));
                 $edit_link = $GLOBALS['egw']->link('/index.php', array('menuaction' => APP_NAME . '.ui_agent.edit', 'id' => $id));
                 $delete_link = $GLOBALS['egw']->link('/index.php', array('menuaction' => APP_NAME . '.ui_agent.delete_agent'));
@@ -152,11 +153,19 @@ class ui_agent extends bo_agent {
      * @return html
      */
     function get_agent_info() {
-        $t = & CreateObject('phpgwapi.Template', EGW_APP_TPL);
+        $pdf = new phpToPDF();
+        $pdf->AddPage();
+        $pdf->SetFont('Times', 'B', 14);
+        $pdf->Image(EGW_INCLUDE_ROOT . '/' . APP_NAME . '/templates/default/images/asecurite.png', 5, 6);
+        $pdf->Ln(30);
+        $pdf->Cell(0, 15, lang("Fiche d'information"), 1, 1, 'C');
+        $pdf->Ln(5);
+
+        /*$t = & CreateObject('phpgwapi.Template', EGW_APP_TPL);
         $t->set_file(array(
             'T_info_agent' => 'info_agent.tpl'
         ));
-        $t->set_block('T_info_agent', 'info_agent');
+        $t->set_block('T_info_agent', 'info_agent');*/
 
         $this->setup_table(APP_NAME, 'egw_asecurite_agent');
         $agent_id = get_var('id');
@@ -169,13 +178,13 @@ class ui_agent extends bo_agent {
                 if (count($f_city_name) == 1) {
                     $f_agent[0]['idasecurite_ville'] = $f_city_name[0]['nom'];
                 }
-                $f_agent[0]['date_debut_contrat'] = $f_agent[0]['date_debut_contrat'] == '' ? '' : $this->format_date( $f_agent[0]['date_debut_contrat']);
-                $f_agent[0]['date_fin_contrat'] = $f_agent[0]['date_fin_contrat'] == '' ? '' : $this->format_date( $f_agent[0]['date_fin_contrat']);
-                $f_agent[0]['date_debut_piece_identite'] = $f_agent[0]['date_debut_piece_identite'] == '' ? '' : $this->format_date( $f_agent[0]['date_debut_piece_identite']);
-                $f_agent[0]['date_fin_piece_identite'] = $f_agent[0]['date_fin_piece_identite'] == '' ? '' : $this->format_date( $f_agent[0]['date_fin_piece_identite']);
+                $f_agent[0]['date_debut_contrat'] = $f_agent[0]['date_debut_contrat'] == '' ? '' : $this->format_date($f_agent[0]['date_debut_contrat']);
+                $f_agent[0]['date_fin_contrat'] = $f_agent[0]['date_fin_contrat'] == '' ? '' : $this->format_date($f_agent[0]['date_fin_contrat']);
+                $f_agent[0]['date_debut_piece_identite'] = $f_agent[0]['date_debut_piece_identite'] == '' ? '' : $this->format_date($f_agent[0]['date_debut_piece_identite']);
+                $f_agent[0]['date_fin_piece_identite'] = $f_agent[0]['date_fin_piece_identite'] == '' ? '' : $this->format_date($f_agent[0]['date_fin_piece_identite']);
 
-                $f_agent[0] = array_map(array('bo_asecurite', 'convert_to_html'), $f_agent[0]);
-                $t->set_var('agent_name', $f_agent[0]['nom'] . ' ' . $f_agent[0]['nom']);
+                //$f_agent[0] = array_map(array('bo_asecurite', 'convert_to_html'), $f_agent[0]);
+                /*$t->set_var('agent_name', $f_agent[0]['nom'] . ' ' . $f_agent[0]['prenom']);
                 $t->set_var('email', $f_agent[0]['email']);
                 $t->set_var('date_naissance', $f_agent[0]['date_naissance']);
                 $t->set_var('adresse', $f_agent[0]['adresse'] . ' ' . $f_agent[0]['code_postal'] . ', ' . $f_agent[0]['idasecurite_ville']);
@@ -188,10 +197,33 @@ class ui_agent extends bo_agent {
                 $t->set_var('date_debut_piece_identite', $f_agent[0]['date_debut_piece_identite']);
                 $t->set_var('date_fin_piece_identite', $f_agent[0]['date_fin_piece_identite']);
                 $t->set_var('commune_piece_identite', $f_agent[0]['commune_piece_identite']);
-                $t->set_var('pays_piece_identite', $f_agent[0]['pays_piece_identite']);
+                $t->set_var('pays_piece_identite', $f_agent[0]['pays_piece_identite']);*/
+                
+                $this->_write_info($pdf, lang("Agent"), strtoupper($f_agent[0]['nom']) . ' ' . ucwords($f_agent[0]['prenom']), 15);
+                $this->_write_info($pdf, lang("Date de naissance"), $f_agent[0]['date_naissance'], 35);
+                $this->_write_info($pdf, lang("Adresse"), $f_agent[0]['adresse'] . ' ' . $f_agent[0]['code_postal'] . ', ' . $f_agent[0]['idasecurite_ville'], 20);
+                $this->_write_info($pdf, lang("Téléphone"), $f_agent[0]['telephone'], 23);
+                $this->_write_info($pdf, lang("Email"), $f_agent[0]['email'], 18);
+                $this->_write_info($pdf, lang("Type de contrat"), $f_agent[0]['type_contrat'], 33);
+                $this->_write_info($pdf, lang("Date de début de contrat"), $f_agent[0]['date_debut_contrat'], 50);
+                $this->_write_info($pdf, lang("Date de fin de contrat"), $f_agent[0]['date_fin_contrat'], 45);
+                $this->_write_info($pdf, lang("Type de pièce d'identité"), $f_agent[0]['type_piece_identite'], 47);
+                $this->_write_info($pdf, lang("Numéro"), $f_agent[0]['numero_piece_identite'], 20);
+                $this->_write_info($pdf, lang("Date de début de validité"), $f_agent[0]['date_debut_piece_identite'], 50);
+                $this->_write_info($pdf, lang("Date de fin de validité"), $f_agent[0]['date_fin_piece_identite'], 45);
+                $this->_write_info($pdf, lang("Commune/Préfecture"), $f_agent[0]['commune_piece_identite'], 43);
+                $this->_write_info($pdf, lang("Pays"), $f_agent[0]['pays_piece_identite'], 12);
             }
         }
-        $t->pparse('out', 'info_agent');
+        //$t->pparse('out', 'info_agent');
+        $pdf->Output();
+    }
+
+    private function _write_info(&$pdf, $label, $info, $label_w=10, $info_w=10) {
+        $pdf->SetFont('Times', 'B', 12);
+        $pdf->Cell($label_w, 10, utf8_decode($label) . ':', 0, 0);
+        $pdf->SetFont('Times', '', 12);
+        $pdf->Cell($info_w, 10, utf8_decode($info) , 0, 1);
     }
 
     /**
@@ -222,7 +254,6 @@ class ui_agent extends bo_agent {
         );
         if (!$this->cities) {
             $js = "opener.location.href='" . ($link = $GLOBALS['egw']->link('/index.php', array('menuaction' => APP_NAME . '.ui_ville.index'))) . "';window.close();";
-
             $content['no_ville_msg'] = "<span id='error'>" . lang('Aucune ville trouvée') . ' <a><button onclick="' . $js . '">' . lang('Créer en ici') . '</button></a>' . " </span>";
         }
         parent::edit($content, $no_button, 'idasecurite_agent', 'Agent', 'egw_asecurite_agent', array('nom', 'prenom', 'date_naissance', 'adresse', 'code_postal', 'idasecurite_ville', 'type_contrat', 'telephone', 'date_debut_contrat', 'date_fin_contrat', 'type_piece_identite', 'numero_piece_identite', 'date_debut_piece_identite', 'date_fin_piece_identite', 'commune_piece_identite', 'pays_piece_identite', 'email'), array('menuaction' => APP_NAME . '.ui_agent.index'));
